e2e5de732552a33725d2edc921888f50e27a69d3
Aboubacar <djarimias@gmail.com>
2023-09-21 01:28:26 +0200

    first init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss")
+
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+      './index.html'
+  ],
+
+  // ...
+}
